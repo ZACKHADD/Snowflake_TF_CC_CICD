1403cfdd3a7cbf4ac4ba0bbbb6a2141689fa5267
zakaria <zakariahaddouche5@gmail.com>
2025-03-11 14:22:29 +0100

    SF configuration

---
+++ provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.87"
+    }
+  }
+}
+
+provider "snowflake" {
+  account = var.SNOWFLAKE_ACCOUNT
+  user        = var.SNOWFLAKE_USER
+  private_key = file("C:/Users/ZAKARIA/.ssh/snowflake_tf_snow_key.p8")
+  authenticator   = "SNOWFLAKE_JWT"
+  role            = "SYSADMIN"
+}
+
+
+
+variable "SNOWFLAKE_ACCOUNT" {
+  default = "zulpzgg-pgb17985"
+}
+
+variable "SNOWFLAKE_USER" {
+  default = "test_role"
+}
