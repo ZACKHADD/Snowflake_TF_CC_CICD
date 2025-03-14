7103a84e204389ff1be2d393fae961dbbb471c82
zakaria <zakariahaddouche5@gmail.com>
2025-03-14 14:43:28 +0100

    config prod

--- environment/main.tf
+++ environment/main.tf
@@ -18,18 +18,18 @@ provider "snowflake" {
 
 
 module "database" {
-  source        = "../../modules/database"
+  source        = "../modules/database"
   database_name = var.database_name
 }
 
 module "warehouse" {
-  source         = "../../modules/warehouse"
+  source         = "../modules/warehouse"
   warehouse_name = var.warehouse_name
   warehouse_size = var.warehouse_size
 }
 
 module "roles" {
-  source         = "../../modules/roles"
+  source         = "../modules/roles"
   role_name      = var.role_name
   database_name  = module.database.database_name
   warehouse_name = module.warehouse.warehouse_name
